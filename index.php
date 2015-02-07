c9f82c7543d90e61450fbd0c1ff5655c5b48c7ea
Refazul Refat <refazul.refat@gmail.com>
2015-02-08 00:53:36 +0600

    minor error

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 	<head>
 		<script type='text/javascript' src='js/jquery-2.1.3.min.js'></script>
 		<script type='text/javascript' src='js/bootstrap.min.js'></script>
-		<link rel='styleshee' href='css/bootstrap.min.css'/>
+		<link rel='stylesheet' href='css/bootstrap.min.css'/>
 	</head>
 	
 	<body>
